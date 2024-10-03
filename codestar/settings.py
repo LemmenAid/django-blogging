f53a6d08807564ad80249adcc4ba4c7d273f2c17
Karlan Lemmen <karlanlemmen@gmail.com>
2024-10-03 13:04:50 +0000

    change secret_key and retrieve key from env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3z3b3jqj0bg&5h=5gv)o22l7%7w_^g$4-sv&@y9iah-72jwmde'
+# SECRET_KEY = 'django-insecure-3z3b3jqj0bg&5h=5gv)o22l7%7w_^g$4-sv&@y9iah-72jwmde'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
